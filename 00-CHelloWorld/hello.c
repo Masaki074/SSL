c5e40c3ce80dced2f9436d0736e311b000d79883
Masaki <masaki4a2014@gmail.com>
2025-04-07 22:12:18 -0300

    Add 00-CHelloWorld

---
+++ 00-CHelloWorld/hello.c
@@ -0,0 +1,13 @@
+/* hello.c
+ * Print "Hello, World!"
+ * MKN
+ * 20250407
+ */
+
+#include <stdio.h>
+
+int main(){
+  printf("Hello, World!");
+
+  return 0;
+}
---
+++ 00-CHelloWorld/readme.md
@@ -0,0 +1,7 @@
+# 00-CHelloWorld
+
+**Compilador seleccionado: GCC**
+
+**Versión del compilador: 14.2.0**
+
+**Versión de C usado: C23**
